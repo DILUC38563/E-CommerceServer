d61e99ed71ff26c52c40987fdaac053d5cb4b85d
Aditya Bokade <85981154+DILUC38563@users.noreply.github.com>
2022-03-05 20:06:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import paymentRoute from "./routes/stripePayment.js";
 
 const app = express();
 dotenv.config();
+app.use(cors());
 
 const PORT = process.env.PORT;
 const URL = process.env.URL;
@@ -31,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.use(cors());
+
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
